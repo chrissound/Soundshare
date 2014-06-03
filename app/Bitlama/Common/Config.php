f9e12dcce0d350ffa2521fb6eecb1eb7cf925f23
chris <chris@soundshare.co.uk>
2014-06-03 08:46:37 +0100

    Config files -- will need to set credentials from this point onwards.

---
+++ app/Bitlama/Common/Config.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Bitlama\Common;
+
+abstract class Config {
+    const projectUrl =          "";
+
+    const dbHost =              "";
+    const dbName =              "";
+    const dbUser =              "";
+    const dbPassword =          ""; 
+    const dbEngine =            "mysql"; 
+
+    const emailHost =           '';
+    const emailPort =           25;
+    const emailUsername =       '';
+    const emailPassword =       '';
+    const emailFrom =           '';
+    const emailFromAddress =    '';
+
+}
